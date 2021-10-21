621d875d3a12d5a6bb425460763157cfdd608c28
jundev <merdeliyev@inbox.ru>
2021-10-21 23:00:06 +0400

    Initial commit

---
+++ src/main/java/az/busdriver/busanddriverrelation/BusAndDriverRelationApplication.java
@@ -0,0 +1,13 @@
+package az.busdriver.busanddriverrelation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusAndDriverRelationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusAndDriverRelationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/busdriver/busanddriverrelation/BusAndDriverRelationApplicationTests.java
@@ -0,0 +1,13 @@
+package az.busdriver.busanddriverrelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusAndDriverRelationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
